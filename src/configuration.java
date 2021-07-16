26513dafe374bab221e9f1cfae9242484f41ca93
Asimiea Sobomate-Victor <85371257+asimiea@users.noreply.github.com>
2021-07-16 13:57:40 -0400

    Create configuration.java

---
+++ src/configuration.java
@@ -0,0 +1,20 @@
+/**
+ * @author Asimiea Sobomate-Victor
+ */
+
+
+import javafx.scene.image.Image;
+
+public class configuration {
+    public static int width = 500;
+    public static int height = 500;
+    public static Image ship = new Image("spaceship.png");
+    public static Image background=new Image("background.png"); //("space.jpg");
+    public static Image bullet=new Image("bullet.png");
+    public static Image asteroid = new Image("asteroid.png");
+
+    public static boolean spaceshipMove=true;
+    public static double spaceshipX=250;
+    public static double spaceshipY=0;
+    
+}
