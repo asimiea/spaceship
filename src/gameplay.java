8ad056d1dc4ed30967162678162844cbf6ff1290
asimiea <asimieasobomate@gmail.com.com>
2021-07-22 11:31:38 -0400

    Still not working

--- src/gameplay.java
+++ src/gameplay.java
@@ -1,49 +1,46 @@
-import java.util.ArrayList;
 import java.util.Random;
 
 import javafx.animation.AnimationTimer;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.util.Duration;
 
 public class Gameplay {
     private Pane root;
     private static Scene scene;
-    private GraphicsContext gc;
-
-    Timeline isDeadLoop;
-    Timeline bulletLoop;
-    Timeline asteroidLoop;
-    Random rand;
+    
+    private Boolean spacePressed;
+    private boolean isBulletFired;
     Boolean gameOver;
-    Background backGround;
-    Spaceship Ship;
-    Asteroid a[];
-    Bullet bullet;   
+    
+    Random rand;
+    
+    //elements
+    private Spaceship Ship;
+    private Asteroid a[];
+    private Bullet bullet;
+    private ImageView life;
+    
+    private int playerLife = 3;
+	private int points;
+
     private AnimationTimer gameTimer;
-    
-    
+        
 
     public Gameplay() throws InterruptedException {
+        rand = new Random();
               
         root = new Pane();
         scene = new Scene(root, configuration.width, configuration.height);
 
-        backGround = new Background(root);
+        new Background(root);
         Ship = new Spaceship(root);
-        a = new Asteroid[10];
+        //a = new Asteroid[10];
         bullet = new Bullet(10,100);
 
         setupKeybinds();
+        createGameElements();
         createGameLoop();        
     }
     
@@ -56,24 +53,81 @@ public class Gameplay {
                 Ship.imageView.setX(Ship.imageView.getX()+10);
             }
             if(e.getCode()==KeyCode.SPACE){
-                shoot();
+                spacePressed = true;
             }
         });
+        scene.setOnKeyReleased(e-> {
+			if (e.getCode() == KeyCode.SPACE) {
+				spacePressed = false;
+				isBulletFired = false;
+			}
+		});
     }
 
+    public void createGameElements() {
+		life = new ImageView(configuration.life);
+		setNewElementPos(life);
+		root.getChildren().addAll(life);
+
+		a = new Asteroid[3];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = new Asteroid(root, 400, 30);
+			setNewElementPos(a[i].imageView);
+			//root.getChildren().add(a[i].imageView);
+		}
+	}
+
+	private void setNewElementPos(ImageView image) {
+		image.setLayoutX(rand.nextInt(configuration.width));
+		image.setLayoutY(-(rand.nextInt(3200) + 600));
+	}
+
+	private void moveElements() {
+		//star.setLayoutY(star.getLayoutY() + 5);
+		life.setLayoutY(life.getLayoutY() + 5);
+		for (int i = 0; i < a.length; i++) {
+			a[i].imageView.setLayoutY(a[i].imageView.getLayoutY() + 7);
+		}
+	}
+
+	private void checkElementsPos() {
+		if (life.getLayoutY() > configuration.height) {
+			setNewElementPos(life);
+		}
+
+		for (int i = 0; i < a.length; i++) {
+			if (a[i].imageView.getLayoutY() > configuration.height) {
+				setNewElementPos(a[i].imageView);
+			}
+		}
+	}
+
+
     private void shoot() {
-        
-        bullet.imageView.setLayoutX(Ship.imageView.getX() + 38);
-		//bullet.imageView.setLayoutY(Ship.imageView.getY() - 45);
-		root.getChildren().add(bullet.imageView);
+        if (spacePressed) {
+			bullet = new Bullet(10, 100);
+			bullet.imageView.setLayoutX(Ship.imageView.getLayoutX() + 38);
+			bullet.imageView.setLayoutY(Ship.imageView.getLayoutY() - 45);
+
+			root.getChildren().add(bullet.imageView);
+			isBulletFired = true;
+		}
     }
+
+    private void moveBullet() {
+		if (isBulletFired) {
+			System.out.println("bullet fired at " + bullet.imageView.getY() + " now moving it");
+			bullet.imageView.setY(bullet.imageView.getY() - 5);
+			System.out.println("bullet is at: " + bullet.imageView.getLayoutY());
+		}
+	}
 
     public void fall(){
 
         for (int i = 0; i < a.length; i++) {
             a[i] = new Asteroid(root, 400, 30);
 			a[i].imageView.setLayoutY(a[i].imageView.getLayoutY() + 7);
-			a[i].imageView.setRotate(a[i].imageView.getRotate() + 4 * i);
+			//a[i].imageView.setRotate(a[i].imageView.getRotate() + 4 * i);
         
         }
     }
@@ -83,26 +137,57 @@ public class Gameplay {
 
 			@Override
 			public void handle(long now) {
-				fall();
-                shoot();
+                //shoot();
+                fall();
+                moveElements();
+				moveBullet();
+				checkElementsPos();
+				//collisionDetection();
 			}
 		};
 		gameTimer.start();
     }
 
-   /* private void add(Entity entity) {
-        entities.add(entity);
-        if (entity instanceof Player) {
-            player = (Player)entity;
-            pane.bindHealth(player.healthPropertyUnmodifiable());
-        }
-        if (entity instanceof Enemy) {
-            enemies.add((Enemy)entity);
-            root.getChildren().add(((Enemy)entity).getHealthBar());
-        }
-        if (entity instanceof Projectile) projectiles.add((Projectile)entity);
-        root.getChildren().add(entity);
-    }*/
+    private void collisionDetection() {
+		// collision detection with life
+		if (37 + 12 > calculateDistance(Ship.imageView.getLayoutX() + 49, Ship.imageView.getLayoutY() + 49, life.getLayoutX() + 37,
+				life.getLayoutY() + 15)) {
+			setNewElementPos(life);
+			addLife();
+		}
+		// collision detection with meteors
+		for (int i = 0; i < a.length; i++) {
+			if (a[i].imageView.getY() + Ship.imageView.getY() > calculateDistance(Ship.imageView.getLayoutX() + 49, Ship.imageView.getLayoutY() + 37,
+					a[i].imageView.getLayoutX() + 20, a[i].imageView.getLayoutY() + 20)) {
+
+				removeLife();
+				setNewElementPos(a[i].imageView);
+			}
+		}
+		/*for (int i = 0; i < a.length; i++) {
+			if (METEOR_RADIUS + Ship.imageView.yProperty() > calculateDistance(Ship.getLayoutX() + 49, Ship.getLayoutY() + 37,
+					greyMeteors[i].getLayoutX() + 20, greyMeteors[i].getLayoutY() + 20)) {
+
+				removeLife();
+				setNewElementPos(greyMeteors[i]);
+			}
+		}*/
+	}
+
+	private void removeLife() {		
+		playerLife--;
+		if (playerLife <= 0) {
+			gameTimer.stop();
+		}
+	}
+
+	private void addLife() {
+		playerLife++;
+	}
+
+	private double calculateDistance(double x1, double y1, double x2, double y2) {
+		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+	}
 
     public static Scene getScene(){
         return scene;
@@ -111,16 +196,4 @@ public class Gameplay {
     public Pane getPane(){
         return root;
     }
-
-    
-    /*public void finishGame() {
-        if(Ship.collide(a[1])){
-            Text textGameOver = new Text(500, 300, " ");
-            textGameOver.prefHeight(100);
-            textGameOver.prefWidth(100);
-            textGameOver.setStyle("-fx-font: 54 arial;");
-            textGameOver.setFill(Color.WHITE);
-            root.getChildren().add(textGameOver);
-        }
-    }*/
 }
